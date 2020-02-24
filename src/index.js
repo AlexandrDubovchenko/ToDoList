9091d63afabc72eb7957f05365a1e40867c1b938
Alexandr <sasha.dubovchenko@gmail.com>
2020-02-24 17:42:57 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+/* eslint-disable prefer-destructuring */
 'use strict';
 // eslint-disable-next-line
 function toDoGenerator(name, $todoForm, $list, $sum, $done, template) {
     this.form = $todoForm;
     this.name = name;
-    this.notes = localStorage.getItem(this.name) ? JSON.parse(localStorage.getItem(this.name)) : [];
+    this.notes = [];
     this.list = $list;
     this.sum = $sum;
     this.done = $done;
     this.template = template;
     this.noteObject = {};
+    this.urlTodo = 'https://todo.hillel.it/todo';
 }
 
 
@@ -25,56 +27,91 @@ toDoGenerator.prototype.formDataToObject = function (data) {
     });
     return obj;
 };
-toDoGenerator.prototype.save = function () {
-    const urlTodo = 'https://todo.hillel.it/todo';
+toDoGenerator.prototype.add = function () {
+    this.noteObjectJson = JSON.stringify(this.noteObject);
     this.todoData = {
         'priority': 1,
-        'value': localStorage.getItem('todo')
+        'value': this.noteObjectJson
     };
-    this.notesJson = JSON.stringify(this.notes);
-    localStorage.setItem(this.name, this.notesJson);
-    this.options = {
+    this.optionsAdd = {
         method: 'POST',
         headers: {
-            'Content-Type' : 'application/json',
+            'Content-Type': 'application/json',
             // eslint-disable-next-line
             'Authorization': 'Bearer ' + login.getToken()
         },
         body: JSON.stringify(this.todoData)
     };
-    fetch(urlTodo, this.options)
+    fetch(this.urlTodo, this.optionsAdd)
         .then(res => res.json()).then();
 };
-toDoGenerator.prototype.createNotesArray = function () {
+toDoGenerator.prototype.createNoteObject = function () {
     this.formData = new FormData(this.form);
-    // eslint-disable-next-line
-    this.formData.append('id', moment().format('x'));
-    this.formData.append('completed', false);
-    // eslint-disable-next-line
-    this.formData.append('time', moment().format('MMMM Do YYYY, h:mm:ss a'));
     this.noteObject = this.formDataToObject(this.formData);
-    this.notes.push(this.noteObject);
-    this.save();
+    this.add();
 };
 toDoGenerator.prototype.setDone = function (id) {
-    this.notes.find(el => el.id === id)
-        .completed = true;
-    this.save();
+    this.noteDone = this.notes.find(el => el._id === +id);
+    const text = JSON.parse(this.noteDone.value).text;
+    const title = JSON.parse(this.noteDone.value).title;
+    this.noteDoneObj = {
+        'priority': 1,
+        'value': JSON.stringify({
+            'text': text,
+            'title': title
+        }),
+        'checked': true
+    };
+    this.optionsDone = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            // eslint-disable-next-line
+            'Authorization': 'Bearer ' + login.getToken()
+        },
+        body: JSON.stringify(this.noteDoneObj)
+    };
+    fetch(`${this.urlTodo}/${id}/toggle`, this.optionsDone).then(() => {
+        this.notesRender(this.list);
+        this.done.textContent = this.getDone();
+    });
 };
 toDoGenerator.prototype.removeNote = function (id, confirm) {
     if (confirm === true) {
-        const index = this.notes.indexOf(this.notes.find(el => el.id === id));
-        // eslint-disable-next-line
-        this.notes.splice(index, 1);
-        this.save();
+        this.optionsDelete = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                // eslint-disable-next-line
+                'Authorization': 'Bearer ' + login.getToken()
+            },
+        };
+        fetch(`${this.urlTodo}/${id}`, this.optionsDelete).then(() => {
+            this.notesRender(this.list);
+            this.done.textContent = this.getDone();
+            this.sum.textContent = this.getSum();
+        });
     }
 };
 toDoGenerator.prototype.editNote = function (id, title, text, confirm) {
     if (confirm) {
-        const index = this.notes.indexOf(this.notes.find(el => el.id === id));
-        this.notes[index].title = title;
-        this.notes[index].text = text;
-        this.save();
+        this.editedNoteObj = {
+            'priority': 1,
+            'value': JSON.stringify({
+                'text': text,
+                'title': title,
+            })
+        };
+        this.optionsEdited = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                // eslint-disable-next-line
+                'Authorization': 'Bearer ' + login.getToken()
+            },
+            body: JSON.stringify(this.editedNoteObj)
+        };
+        fetch(`${this.urlTodo}/${id}`, this.optionsEdited).then(this.notesRender(this.list));
     }
 
 };
@@ -85,27 +122,41 @@ toDoGenerator.prototype.getDone = function () {
     return this.notes.filter((el) => el.completed === true).length;
 };
 toDoGenerator.prototype.renderEditor = function (note, id) {
+    this.valueEdit = JSON.parse(this.notes.find(el => el._id === +id).value);
     note.innerHTML =
-        `<input type="text" name="title" value="${this.notes.find(el => el.id === id).title}">
+        `<input type="text" name="title" value="${this.valueEdit.title}">
                 
-                <textarea rows="5"  name="text"  >${this.notes.find(el => el.id === id).text}</textarea>
-                <button type="button" class="note_save">Save</button>`;
+                <textarea rows="5"  name="text"  >${this.valueEdit.text}</textarea>
+                <button type="button" class="note_add">add</button>`;
 
 
 };
 toDoGenerator.prototype.notesRender = function (container) {
-    container.innerHTML = '';
-    this.notes.forEach(note => {
-        container.innerHTML += this.template(note);
+    fetch(this.urlTodo, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            // eslint-disable-next-line
+            'Authorization': 'Bearer ' + login.getToken()
+        }
+    }).then(res => res.json()).then(res => {
+        this.notes = res;
+        container.innerHTML = '';
+        this.notes.forEach(note => {
+            const id = note._id;
+            const checked = note.checked;
+            const value = JSON.parse(note.value);
+            container.innerHTML += this.template(value, id, checked);
+        });
     });
 };
 toDoGenerator.prototype.init = function () {
+    this.notesRender(this.list);
     this.sum.textContent = this.getSum();
     this.done.textContent = this.getDone();
-    this.notesRender(this.list);
     this.form.addEventListener('submit', e => {
         e.preventDefault();
-        this.createNotesArray();
+        this.createNoteObject();
         this.notesRender(this.list);
         this.form.reset();
         this.sum.textContent = this.getSum();
@@ -113,24 +164,20 @@ toDoGenerator.prototype.init = function () {
     this.list.addEventListener('click', e => {
         if (e.target.classList.contains('note_done')) {
             this.setDone(e.target.closest('li').id);
-            this.notesRender(this.list);
-            this.done.textContent = this.getDone();
+
         }
         if (e.target.classList.contains('note_remove')) {
             this.removeNote(e.target.closest('li').id, confirm('Sure'));
-            this.notesRender(this.list);
-            this.done.textContent = this.getDone();
-            this.sum.textContent = this.getSum();
+
         }
         if (e.target.classList.contains('note_edit')) {
             this.renderEditor(e.target.closest('li'), e.target.closest('li').id);
 
         }
-        if (e.target.classList.contains('note_save')) {
+        if (e.target.classList.contains('note_add')) {
             const inputTitle = e.target.parentNode.querySelector('[name="title"]');
             const inputText = e.target.parentNode.querySelector('[name="text"]');
             this.editNote(e.target.parentNode.id, inputTitle.value, inputText.value, confirm('Sure?'));
-            this.notesRender(this.list);
         }
     });
 
@@ -147,18 +194,18 @@ const toDoList = new toDoGenerator(
     $list,
     $sum,
     $done,
-    note => `<li class="notes_item ${note.completed ? 'completed' : ''}" id = ${note.id}  > 
+    (note, id, checked) => `<li class="notes_item ${checked ? 'completed' : ''}" id = ${id}  > 
                     <h4 class="note_title">${note.title}</h4>
                     <p class="note_text">${note.text}</p>
                     <div class="buttons">
-                        <button type="button" class="note_done" ${note.completed ? 'disabled' : ''} >Done</button>
+                        <button type="button" class="note_done" ${checked ? 'disabled' : ''} >Done</button>
                         <button type="button" class="note_remove"  >Remove</button>
-                        <button type="button" class="note_edit"  ${note.completed ? 'disabled' : ''} >Edit</button>
+                        <button type="button" class="note_edit"  ${checked ? 'disabled' : ''} >Edit</button>
                     </div>
                     
                 </li>`
 );
-toDoList.init();
+
 
 function Login($authorizationForm) {
     this.form = $authorizationForm;
@@ -181,6 +228,9 @@ Login.prototype.authorization = function () {
             this.form.classList.add('hidden');
             localStorage.setItem(this.tokenKey, res['access_token']);
             $todoContainer.classList.remove('hidden');
+            if (this.getToken()) {
+                toDoList.init();
+            }
         });
 };
 Login.prototype.getFormData = function () {
@@ -191,7 +241,7 @@ Login.prototype.getFormData = function () {
     this.formData.forEach(value => this.formDataObj.value += value);
     return this.formDataObj;
 };
-Login.prototype.getToken = function(){
+Login.prototype.getToken = function () {
     return localStorage.getItem(this.tokenKey);
 };
 Login.prototype.init = function () {
